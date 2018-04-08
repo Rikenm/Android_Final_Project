1f001c250221e55d1fc316bb0458b3bf292c87ca
Riken Maharjan <rikenm@bu.edu>
2018-04-07 22:29:04 -0400

    add new activities and fragment with its layout

---
+++ app/src/main/java/com/rikenmaharjan/y2yc/activities/DetailsActivity.java
@@ -0,0 +1,8 @@
+package com.rikenmaharjan.y2yc.activities;
+
+/**
+ * Created by Riken on 4/7/18.
+ */
+
+public class DetailsActivity {
+}
---
+++ app/src/main/java/com/rikenmaharjan/y2yc/fragments/LotteryFragment.java
@@ -0,0 +1,8 @@
+package com.rikenmaharjan.y2yc.fragments;
+
+/**
+ * Created by Riken on 4/7/18.
+ */
+
+public class LotteryFragment {
+}
